dd016037982b2ef8666debecfbf47a3055ea4eb2
mohaise1 <sami.mohaisen@mail.utoronto.ca>
2025-05-13 18:15:40 -0400

    hw1 done

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
 
         list.add(1001);
         list.add(4);
